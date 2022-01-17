d726a000aaab6b0726bfe2781dffbddc57c13125
潘 彥良 <saber30092@gmail.com>
2022-01-17 17:45:10 +0800

    BlogIndexFinish
    部落格主頁面初始完成

--- FireBlogs/src/views/Home.vue
+++ FireBlogs/src/views/Home.vue
@@ -1,12 +1,43 @@
 <template>
   <div class="home">
-
+    <BlogPost v-bind:post="welcomeScreen" />
+    <BlogPost
+      v-bind:post="post"
+      v-for="(post, index) in sampleBlogPost"
+      v-bind:key="index"
+    />
   </div>
 </template>
 
 <script>
+import BlogPost from "../components/BlogPost";
 export default {
   name: "Home",
-  components: {},
+  components: {
+    BlogPost,
+  },
+  data() {
+    return {
+      welcomeScreen: {
+        title: "Welcome",
+        blogPost:
+          "Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post!",
+        welcomeScreen: true,
+        photo: "coding",
+      },
+      sampleBlogPost: [
+        {
+          title: "This is a Filler Title",
+          blogHTML: "This is a filler blog post title!",
+          blogCoverPhoto: "beautiful-stories",
+        },
+        {
+          title: "This is a Filler Title",
+          blogHTML: "This is a filler blog post title!",
+          blogCoverPhoto: "designed-for-everyone",
+        },
+      ],
+    };
+  },
 };
 </script>
