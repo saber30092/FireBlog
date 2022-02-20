b0a8b88daece62169d4a8d85c987c54a3eb26655
saber30092 <80209671+saber30092@users.noreply.github.com>
2022-02-20 17:07:28 +0800

    forgotpassword
    新增忘記密碼view

--- FireBlogs/src/firebase/firebaseInit.js
+++ FireBlogs/src/firebase/firebaseInit.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAKxdr4rmjAlZ1D9oiiZ3G6Fb7tshENetI",
-    authDomain: "fireblogsyt-18236.firebaseapp.com",
-    projectId: "fireblogsyt-18236",
-    storageBucket: "fireblogsyt-18236.appspot.com",
-    messagingSenderId: "837190319238",
-    appId: "1:837190319238:web:6ec438fb059ff2edade507"
-  };
+  apiKey: "AIzaSyAA6IGmiE5TIKKv48-lA85lMuTHyN-G1Fw",
+  authDomain: "fireblogs-387d9.firebaseapp.com",
+  projectId: "fireblogs-387d9",
+  storageBucket: "fireblogs-387d9.appspot.com",
+  messagingSenderId: "139189560078",
+  appId: "1:139189560078:web:0e5de9a4ef739625d52d5f"
+};
 
   const fierbaseApp=firebase.initializeApp(firebaseConfig);
   const timestamp=firebase.firestore.FieldValue.serverTimestamp;
